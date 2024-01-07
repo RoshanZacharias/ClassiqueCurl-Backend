a7dc9ef89cb5ee8955c4845b1d5377ca45906e9f
Roshan Zacharias <roshanzacharias20@gmail.com>
2024-01-07 16:24:45 +0530

    asgi file updated

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,21 +1,16 @@
 import os
-import django
-django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing as routingchat
 from salon import routing as routingNotification
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         'websocket': URLRouter(routingchat.websocket_urlpatterns + 
             routingNotification.websocket_urlpatterns),
-
     }
 )
