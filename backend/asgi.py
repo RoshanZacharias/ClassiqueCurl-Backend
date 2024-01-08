2bdf1ead2e95b54a8b27688a1f7e6627ca4c2a6c
Roshan Zacharias <roshanzacharias20@gmail.com>
2024-01-08 12:17:20 +0530

    asgi updated

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,12 +1,13 @@
 import os
 import django
-django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing as routingchat
 from salon import routing as routingNotification
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 
 django_asgi_app = get_asgi_application()
 
