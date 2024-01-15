3f4538150d40f6872989b576eff4b7c78f90d324
Roshan Zacharias <roshanzacharias20@gmail.com>
2024-01-15 15:08:16 +0530

    channel layers updated

--- backend/settings.py
+++ backend/settings.py
@@ -43,13 +43,20 @@ INSTALLED_APPS = [
 ]
 
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
-
-
 
 
 REST_FRAMEWORK = {
